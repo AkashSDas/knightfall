61ad4a65841a1dcb2548b2e6e746a9b0bc4b96c9
AkashSDas <aakashdas368@gmail.com>
2024-07-01 22:12:23 +0530

    chore: set up backend

---
+++ backend/src/utils/swagger.ts
@@ -0,0 +1,15 @@
+import swaggerJSdoc from "swagger-jsdoc";
+import { version } from "../../package.json";
+
+const options: swaggerJSdoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Knightfall API",
+            version,
+        },
+    },
+    apis: ["./src/routes/*.ts"],
+};
+
+export const swaggerSpec = swaggerJSdoc(options);
