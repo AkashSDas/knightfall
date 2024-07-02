57048556de73382a7baa181d46d53bb13dd30544
AkashSDas <aakashdas368@gmail.com>
2024-07-02 20:18:28 +0530

    chore: add tmp test case

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export default function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <h1>Knightfall</h1>
+
+            <button onClick={() => setCount((prev) => prev + 1)}>
+                {count}
+            </button>
         </div>
     );
 }
