fd38754764e754719dbd33c78606f0cba50c9e9c
AkashSDas <aakashdas368@gmail.com>
2024-07-03 09:48:35 +0530

    chore: add chakra-ui and fontawesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { useState } from "react";
+import { theme } from "./lib/chakra";
 
 export default function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
+        <ChakraProvider theme={theme}>
             <h1>Knightfall</h1>
 
             <button onClick={() => setCount((prev) => prev + 1)}>
                 {count}
             </button>
-        </div>
+        </ChakraProvider>
     );
 }
