a549de1914cfc28f766fdbc0f99ac9bec5983aaa
AkashSDas <aakashdas368@gmail.com>
2024-07-08 23:24:59 +0530

    create CancelOAuthText and CompleteOAuthSignupForm

---
+++ frontend/src/components/auth/CancelOAuthText.tsx
@@ -0,0 +1,51 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { authService } from "../../services/auth";
+
+export function CancelOAuthText(props: { email: string | null | undefined }) {
+    const queryClient = useQueryClient();
+
+    const cancelOAuthMutation = useMutation({
+        async mutationFn() {
+            await authService.cancelOAuthSignup();
+            await Promise.all([
+                queryClient.refetchQueries({ queryKey: ["loggedInUser"] }),
+                queryClient.refetchQueries({ queryKey: ["accessToken"] }),
+            ]);
+        },
+    });
+
+    return (
+        <Text color="gray.300">
+            Your Google account{" "}
+            <Text as="span" fontWeight="700" color="gray.200">
+                {props.email}
+            </Text>{" "}
+            will be connected to your new Knightfall account. Wrong identity?{" "}
+            <Text
+                color="blue.400"
+                as={Button}
+                bgColor="transparent"
+                variant="text"
+                border="none"
+                p="0"
+                h="fit-content"
+                _hover={{
+                    bgColor: "transparent",
+                    textDecor: "underline",
+                }}
+                _active={{
+                    bgColor: "transparent",
+                    textDecor: "underline",
+                }}
+                fontWeight="700"
+                cursor="pointer"
+                onClick={() => {
+                    cancelOAuthMutation.mutateAsync();
+                }}
+            >
+                {cancelOAuthMutation.isPending ? "Cancelling..." : "Start over"}
+            </Text>
+        </Text>
+    );
+}
---
+++ frontend/src/components/auth/CompleteOAuthSignupForm.tsx
@@ -0,0 +1,117 @@
+import {
+    Button,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+    VStack,
+    useToast,
+} from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { object, string, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authService } from "../../services/auth";
+import { useUser } from "../../hooks/auth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const schema = object({
+    username: string({ required_error: "Required" })
+        .min(2, { message: "Too short" })
+        .max(256, { message: "Too long" }),
+});
+
+export type CompleteOAuthSignupInputs = z.infer<typeof schema>;
+
+export function CompleteOAuthSignupForm() {
+    const toast = useToast();
+    const { user } = useUser();
+    const form = useForm<CompleteOAuthSignupInputs>({
+        defaultValues: { username: "" },
+        resolver: zodResolver(schema),
+    });
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        async mutationFn(payload: CompleteOAuthSignupInputs) {
+            const [, err] = await authService.completeOAuthSignup(payload);
+            if (err) {
+                toast({
+                    title: "Error",
+                    description: err.message,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            } else {
+                toast({
+                    title: "Success",
+                    description: "Account created successfully",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                navigate("/");
+                await Promise.all([
+                    queryClient.refetchQueries({ queryKey: ["loggedInUser"] }),
+                    queryClient.refetchQueries({ queryKey: ["accessToken"] }),
+                ]);
+            }
+        },
+    });
+
+    const completeOAuthSignup = form.handleSubmit(
+        async (data) => await mutation.mutateAsync(data)
+    );
+
+    useEffect(
+        function () {
+            if (user?.username) {
+                form.setValue("username", user.username);
+            }
+        },
+        [user, form]
+    );
+
+    return (
+        <VStack as="form" gap="1rem" w="100%" onSubmit={completeOAuthSignup}>
+            <FormControl
+                isInvalid={form.formState.errors.username ? true : false}
+                mb="0.5rem"
+                isRequired
+            >
+                <FormLabel fontSize="14px">Username</FormLabel>
+                <Input
+                    variant="contained"
+                    {...form.register("username")}
+                    placeholder="What to call you champion?"
+                />
+                <FormErrorMessage variant="solid" fontWeight="bold">
+                    {form.formState.errors.username?.message}
+                </FormErrorMessage>
+            </FormControl>
+
+            <FormControl mb="0.5rem" isRequired>
+                <FormLabel fontSize="14px">Email</FormLabel>
+                <Input
+                    variant="contained"
+                    defaultValue={user?.email ?? ""}
+                    disabled
+                />
+            </FormControl>
+
+            <Button
+                type="submit"
+                variant="primary"
+                isLoading={mutation.isPending}
+                h="48px"
+                w="100%"
+                onMouseDown={() => form.trigger()}
+            >
+                Complete Signup
+            </Button>
+        </VStack>
+    );
+}
