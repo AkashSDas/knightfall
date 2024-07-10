276ce4abe5d1c832555f066ad3fd7443e4e9ab5d
AkashSDas <aakashdas368@gmail.com>
2024-07-10 20:23:12 +0530

    create controllers for get logged in user notifications and mark notifications as seen

---
+++ backend/src/controllers/notification.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import * as schemas from "../schema/notification";
+import { UserDocument } from "../models/user";
+import { Notification } from "../models/notification";
+import {
+    GET_LOGGED_IN_USER_NOTIFICATIONS_LIMIT,
+    GET_LOGGED_IN_USER_NOTIFICATIONS_OFFSET,
+} from "../utils/notification";
+
+/**
+ * Get notifications of logged in user from latest to oldest in a paginated fashion
+ **/
+export async function getLoggedInUserNotificationsCtrl(
+    req: Request<
+        unknown,
+        unknown,
+        unknown,
+        schemas.GetLoggedInUserNotificationsSchema["query"]
+    >,
+    res: Response,
+) {
+    const { limit, offset } = req.query;
+    const userId = (req.user as UserDocument)._id;
+
+    const now = new Date();
+
+    const filter = {
+        user: userId,
+        $or: [{ maxAge: { $exists: false } }, { maxAge: { $gt: now } }],
+    };
+
+    const [totalCount, notifications] = await Promise.all([
+        Notification.countDocuments(filter),
+        Notification.find(filter)
+            .sort({ createdAt: -1 })
+            .skip(offset ?? GET_LOGGED_IN_USER_NOTIFICATIONS_OFFSET)
+            .limit(limit ?? GET_LOGGED_IN_USER_NOTIFICATIONS_LIMIT),
+    ]);
+
+    // Setting the X-Total-Count header
+    res.set("X-Total-Count", totalCount.toString());
+
+    return res.status(200).json({ notifications });
+}
+
+export async function markNotificationsAsSeenCtrl(
+    req: Request<
+        unknown,
+        unknown,
+        unknown,
+        schemas.MarkNotificationsAsSeenSchema["query"]
+    >,
+    res: Response,
+) {
+    const userId = (req.user as UserDocument)._id;
+    const { markAll, ids } = req.query;
+
+    if (markAll) {
+        await Notification.updateMany({ userId, seen: false }, { seen: true });
+    } else {
+        await Notification.updateMany(
+            { _id: { $in: ids }, userId, seen: false },
+            { seen: true },
+        );
+    }
+
+    return res.status(204).end();
+}
---
+++ backend/src/utils/notification.ts
@@ -0,0 +1,5 @@
+/** Default limit of notificiations to fetch in `getLoggedInUserNotificationsCtrl` */
+export const GET_LOGGED_IN_USER_NOTIFICATIONS_LIMIT = 10;
+
+/** Default offset of notificiations to fetch in `getLoggedInUserNotificationsCtrl` */
+export const GET_LOGGED_IN_USER_NOTIFICATIONS_OFFSET = 0;
