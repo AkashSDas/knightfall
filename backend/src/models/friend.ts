dfcaa9a8fd53efa7a6d214cf4b2fd545b8b0b7cb
AkashSDas <aakashdas368@gmail.com>
2024-07-14 12:53:39 +0530

    create friend model and schemas for endpoint operations

---
+++ backend/src/models/friend.ts
@@ -0,0 +1,58 @@
+import {
+    Ref,
+    Severity,
+    getModelForClass,
+    modelOptions,
+    prop,
+} from "@typegoose/typegoose";
+import { UserDocument } from "./user";
+import { Types } from "mongoose";
+
+export const FRIEND_REQUEST_STATUS = {
+    PENDING: "pending",
+    ACCEPTED: "accepted",
+    REJECTED: "rejected",
+    BLOCKED: "blocked",
+} as const;
+
+@modelOptions({
+    schemaOptions: {
+        timestamps: true,
+        toJSON: { virtuals: true },
+        typeKey: "type",
+    },
+    options: { allowMixed: Severity.ALLOW, customName: "friend" },
+})
+export class FriendDocument {
+    // =================================
+    // Fields
+    // =================================
+
+    @prop({ ref: () => UserDocument, required: true })
+    fromUser: Ref<UserDocument>;
+
+    @prop({ ref: () => UserDocument, required: true })
+    toUser: Ref<UserDocument>;
+
+    @prop({
+        type: String,
+        enum: Object.values(FRIEND_REQUEST_STATUS),
+        default: FRIEND_REQUEST_STATUS.PENDING,
+        required: true,
+    })
+    status: (typeof FRIEND_REQUEST_STATUS)[keyof typeof FRIEND_REQUEST_STATUS];
+
+    // =================================
+    // Virtuals
+    // =================================
+
+    _id!: Types.ObjectId;
+
+    /** Get transformed MongoDB `_id` */
+    get id() {
+        return this._id.toHexString();
+    }
+}
+
+/** User Typegoose Model */
+export const Friend = getModelForClass(FriendDocument);
