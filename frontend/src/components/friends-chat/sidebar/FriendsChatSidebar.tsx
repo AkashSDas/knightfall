6e41e23e0d1fa796457937f450af17a5b688c3d5
AkashSDas <aakashdas368@gmail.com>
2024-07-20 13:59:30 +0530

    fix content flowing in sidebar

--- frontend/src/components/friends-chat/sidebar/FriendsChatSidebar.tsx
+++ frontend/src/components/friends-chat/sidebar/FriendsChatSidebar.tsx
@@ -74,155 +74,179 @@ export function Sidebar() {
                 initial={{ translateX: "0" }}
                 animate={controls}
                 role="group"
-                px={isSidebarOpen ? "8px" : "40px"}
+                px="8px"
             >
-                {/* Open and close sidebar buttons */}
-
-                <Tooltip
-                    label={"Close sidebar"}
-                    openDelay={200}
-                    placement="right"
-                >
-                    <IconButton
-                        opacity={0}
-                        _groupHover={{ opacity: isSidebarOpen ? 1 : 0 }}
-                        pointerEvents={isSidebarOpen ? "initial" : "none"}
-                        variant="ghost"
-                        aria-label="Close sidebar"
-                        color="gray.200"
-                        h="28px"
-                        right="4px"
-                        top="4px"
-                        borderRadius="8px"
-                        px="8px"
-                        pos="absolute"
-                        transition="all 0.2s cubic-bezier(0.4, 0, 0.2, 1)"
-                        _active={{ bgColor: "gray.600" }}
-                        onClick={() => {
-                            controls.start({
-                                translateX: "-200px",
-                                transition: { ease: "easeInOut" },
-                            });
-                            dispatch(friendsChatActions.setSidebarOpen(false));
-                        }}
-                    >
-                        <FontAwesomeIcon icon={faAngleDoubleLeft} size="sm" />
-                    </IconButton>
-                </Tooltip>
-
-                <Tooltip
-                    label={"Open sidebar"}
-                    openDelay={200}
-                    placement="right"
-                >
-                    <IconButton
-                        opacity={isSidebarOpen ? 0 : 1}
-                        pointerEvents={!isSidebarOpen ? "initial" : "none"}
-                        variant="ghost"
-                        aria-label="Close sidebar"
-                        color="gray.200"
-                        h="38px"
-                        right="4px"
-                        top="50%"
-                        pos="absolute"
-                        transition="all 0.2s cubic-bezier(0.4, 0, 0.2, 1)"
-                        _active={{ bgColor: "gray.600" }}
-                        onClick={() => {
-                            controls.start({
-                                translateX: "0px",
-                                transition: { ease: "easeInOut" },
-                            });
-                            dispatch(friendsChatActions.setSidebarOpen(true));
-                        }}
-                        minW="28px"
-                        borderRadius="6px"
-                    >
-                        <FontAwesomeIcon icon={faGripLinesVertical} size="sm" />
-                    </IconButton>
-                </Tooltip>
-
-                {/* Top content */}
-
-                <VStack alignItems="start" w="100%" my="2rem" gap={0}>
+                {isSidebarOpen ? null : (
                     <Tooltip
-                        label="Search your friends"
+                        label={"Open sidebar"}
                         openDelay={200}
                         placement="right"
                     >
-                        <Button
+                        <IconButton
+                            opacity={isSidebarOpen ? 0 : 1}
+                            pointerEvents={!isSidebarOpen ? "initial" : "none"}
                             variant="ghost"
-                            leftIcon={
-                                <FontAwesomeIcon
-                                    icon={faSearch}
-                                    size="sm"
-                                    style={{ marginRight: "8px" }}
-                                />
-                            }
-                            fontSize="16px"
-                            w="100%"
-                            _active={{ bgColor: "gray.600" }}
-                            justifyContent="start"
-                            transition="all 0.2s ease-in-out"
+                            aria-label="Close sidebar"
+                            color="gray.200"
                             h="38px"
-                            borderRadius="8px"
-                            onClick={() => openContent({ type: "search" })}
-                        >
-                            Search
-                        </Button>
-                    </Tooltip>
-
-                    <Tooltip label="Inbox" openDelay={200} placement="right">
-                        <Button
-                            variant="ghost"
-                            leftIcon={
-                                <FontAwesomeIcon
-                                    icon={faInbox}
-                                    size="sm"
-                                    style={{ marginRight: "8px" }}
-                                />
-                            }
-                            fontSize="16px"
-                            w="100%"
+                            right="4px"
+                            top="50%"
+                            pos="absolute"
+                            transition="all 0.2s cubic-bezier(0.4, 0, 0.2, 1)"
                             _active={{ bgColor: "gray.600" }}
-                            justifyContent="start"
-                            h="38px"
-                            borderRadius="8px"
-                            onClick={() =>
-                                openContent({ type: "friendRequests" })
-                            }
+                            onClick={() => {
+                                controls.start({
+                                    translateX: "0px",
+                                    transition: { ease: "easeInOut" },
+                                });
+                                dispatch(
+                                    friendsChatActions.setSidebarOpen(true)
+                                );
+                            }}
+                            minW="28px"
+                            borderRadius="6px"
                         >
-                            Friend Requests
-                        </Button>
+                            <FontAwesomeIcon
+                                icon={faGripLinesVertical}
+                                size="sm"
+                            />
+                        </IconButton>
                     </Tooltip>
+                )}
 
-                    <Tooltip
-                        label="Blocked users"
-                        openDelay={200}
-                        placement="right"
-                    >
-                        <Button
-                            variant="ghost"
-                            leftIcon={
+                {!isSidebarOpen ? null : (
+                    <>
+                        <Tooltip
+                            label={"Close sidebar"}
+                            openDelay={200}
+                            placement="right"
+                        >
+                            <IconButton
+                                opacity={0}
+                                _groupHover={{ opacity: isSidebarOpen ? 1 : 0 }}
+                                pointerEvents={
+                                    isSidebarOpen ? "initial" : "none"
+                                }
+                                variant="ghost"
+                                aria-label="Close sidebar"
+                                color="gray.200"
+                                h="28px"
+                                right="4px"
+                                top="4px"
+                                borderRadius="8px"
+                                px="8px"
+                                pos="absolute"
+                                transition="all 0.2s cubic-bezier(0.4, 0, 0.2, 1)"
+                                _active={{ bgColor: "gray.600" }}
+                                onClick={() => {
+                                    controls.start({
+                                        translateX: "-200px",
+                                        transition: { ease: "easeInOut" },
+                                    });
+                                    dispatch(
+                                        friendsChatActions.setSidebarOpen(false)
+                                    );
+                                }}
+                            >
                                 <FontAwesomeIcon
-                                    icon={faBan}
+                                    icon={faAngleDoubleLeft}
                                     size="sm"
-                                    style={{ marginRight: "8px" }}
                                 />
-                            }
-                            fontSize="16px"
-                            w="100%"
-                            _active={{ bgColor: "gray.600" }}
-                            justifyContent="start"
-                            h="38px"
-                            borderRadius="8px"
-                            onClick={() => openContent({ type: "blocked" })}
-                        >
-                            Blocked
-                        </Button>
-                    </Tooltip>
-                </VStack>
+                            </IconButton>
+                        </Tooltip>
 
-                <FriendsList />
+                        {/* Top content */}
+
+                        <VStack alignItems="start" w="100%" my="2rem" gap={0}>
+                            <Tooltip
+                                label="Search your friends"
+                                openDelay={200}
+                                placement="right"
+                            >
+                                <Button
+                                    variant="ghost"
+                                    leftIcon={
+                                        <FontAwesomeIcon
+                                            icon={faSearch}
+                                            size="sm"
+                                            style={{ marginRight: "8px" }}
+                                        />
+                                    }
+                                    fontSize="16px"
+                                    w="100%"
+                                    _active={{ bgColor: "gray.600" }}
+                                    justifyContent="start"
+                                    transition="all 0.2s ease-in-out"
+                                    h="38px"
+                                    borderRadius="8px"
+                                    onClick={() =>
+                                        openContent({ type: "search" })
+                                    }
+                                >
+                                    Search
+                                </Button>
+                            </Tooltip>
+
+                            <Tooltip
+                                label="Inbox"
+                                openDelay={200}
+                                placement="right"
+                            >
+                                <Button
+                                    variant="ghost"
+                                    leftIcon={
+                                        <FontAwesomeIcon
+                                            icon={faInbox}
+                                            size="sm"
+                                            style={{ marginRight: "8px" }}
+                                        />
+                                    }
+                                    fontSize="16px"
+                                    w="100%"
+                                    _active={{ bgColor: "gray.600" }}
+                                    justifyContent="start"
+                                    h="38px"
+                                    borderRadius="8px"
+                                    onClick={() =>
+                                        openContent({ type: "friendRequests" })
+                                    }
+                                >
+                                    Friend Requests
+                                </Button>
+                            </Tooltip>
+
+                            <Tooltip
+                                label="Blocked users"
+                                openDelay={200}
+                                placement="right"
+                            >
+                                <Button
+                                    variant="ghost"
+                                    leftIcon={
+                                        <FontAwesomeIcon
+                                            icon={faBan}
+                                            size="sm"
+                                            style={{ marginRight: "8px" }}
+                                        />
+                                    }
+                                    fontSize="16px"
+                                    w="100%"
+                                    _active={{ bgColor: "gray.600" }}
+                                    justifyContent="start"
+                                    h="38px"
+                                    borderRadius="8px"
+                                    onClick={() =>
+                                        openContent({ type: "blocked" })
+                                    }
+                                >
+                                    Blocked
+                                </Button>
+                            </Tooltip>
+                        </VStack>
+
+                        <FriendsList />
+                    </>
+                )}
             </VStack>
         </AnimatePresence>
     );
