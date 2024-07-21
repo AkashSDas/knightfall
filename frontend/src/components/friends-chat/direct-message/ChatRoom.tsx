9b3119bcf6ae169fb5659a63a2c265a14f29fff7
AkashSDas <aakashdas368@gmail.com>
2024-07-21 19:22:13 +0530

    remove redundant imports

--- frontend/src/components/friends-chat/direct-message/ChatRoom.tsx
+++ frontend/src/components/friends-chat/direct-message/ChatRoom.tsx
@@ -24,7 +24,6 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faArrowDownLong,
-    faChampagneGlasses,
     faPaperPlane,
 } from "@fortawesome/free-solid-svg-icons";
 
