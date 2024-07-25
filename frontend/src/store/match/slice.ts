06f89f0515a14da96f7049cecd0cf291b561a9a4
AkashSDas <aakashdas368@gmail.com>
2024-07-25 20:52:45 +0530

    create select action for chess moves

--- frontend/src/store/match/slice.ts
+++ frontend/src/store/match/slice.ts
@@ -1,14 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { CHESS_PIECES, CHESS_PIECE_COLOR } from "../../utils/chess";
-import { v4 as uuid } from "uuid";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import {
+    CHESS_PIECES,
+    CHESS_PIECE_COLOR,
+    getInitialChessBoard,
+    getValidMovesForPiece,
+} from "../../utils/chess";
 import { RootState } from "..";
 
-type ChessPiece = {
+export type ChessPiece = {
     type: (typeof CHESS_PIECES)[keyof typeof CHESS_PIECES];
     color: (typeof CHESS_PIECE_COLOR)[keyof typeof CHESS_PIECE_COLOR];
 };
 
-type ChessBlock = {
+export type ChessBlock = {
     id: string;
     piece: ChessPiece | null;
     color: (typeof CHESS_PIECE_COLOR)[keyof typeof CHESS_PIECE_COLOR];
@@ -16,111 +20,6 @@ type ChessBlock = {
     showPath: boolean;
     showExplosion: boolean;
 };
-
-function getInitialChessBoard(): ChessBlock[][] {
-    const board: ChessBlock[][] = [];
-
-    // Create a board with colors
-    for (let i = 0; i < 8; i++) {
-        board.push([]);
-        for (let j = 0; j < 8; j++) {
-            const color =
-                (i + j) % 2 == 0
-                    ? CHESS_PIECE_COLOR.BLACK
-                    : CHESS_PIECE_COLOR.WHITE;
-
-            board[i].push({
-                id: uuid(),
-                piece: null,
-                selected: false,
-                color: color,
-                showPath: true,
-                showExplosion: false,
-            });
-        }
-    }
-
-    // Add chess pieces
-    board[0][0].piece = {
-        type: CHESS_PIECES.ROOK,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][1].piece = {
-        type: CHESS_PIECES.KNIGHT,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][2].piece = {
-        type: CHESS_PIECES.BISHOP,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][3].piece = {
-        type: CHESS_PIECES.QUEEN,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][4].piece = {
-        type: CHESS_PIECES.KING,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][5].piece = {
-        type: CHESS_PIECES.BISHOP,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][6].piece = {
-        type: CHESS_PIECES.KNIGHT,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-    board[0][7].piece = {
-        type: CHESS_PIECES.ROOK,
-        color: CHESS_PIECE_COLOR.BLACK,
-    };
-
-    board[7][0].piece = {
-        type: CHESS_PIECES.ROOK,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][1].piece = {
-        type: CHESS_PIECES.KNIGHT,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][2].piece = {
-        type: CHESS_PIECES.BISHOP,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][3].piece = {
-        type: CHESS_PIECES.QUEEN,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][4].piece = {
-        type: CHESS_PIECES.KING,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][5].piece = {
-        type: CHESS_PIECES.BISHOP,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][6].piece = {
-        type: CHESS_PIECES.KNIGHT,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-    board[7][7].piece = {
-        type: CHESS_PIECES.ROOK,
-        color: CHESS_PIECE_COLOR.WHITE,
-    };
-
-    // Add pawns
-    for (let i = 0; i < 8; i++) {
-        board[1][i].piece = {
-            type: CHESS_PIECES.PAWN,
-            color: CHESS_PIECE_COLOR.BLACK,
-        };
-        board[6][i].piece = {
-            type: CHESS_PIECES.PAWN,
-            color: CHESS_PIECE_COLOR.WHITE,
-        };
-    }
-
-    return board;
-}
 
 export type MatchState = {
     board: ChessBlock[][];
@@ -144,16 +43,113 @@ export const matchSlice = createSlice({
                     ? CHESS_PIECE_COLOR.WHITE
                     : CHESS_PIECE_COLOR.BLACK;
         },
-        selectPiece: (state, action) => {
-            state.board.forEach((row) => {
-                row.forEach((block) => {
-                    block.selected = false;
-                });
-            });
+        selectPiece: (
+            state,
+            action: PayloadAction<{ rowIndex: number; colIndex: number }>
+        ) => {
+            const { rowIndex, colIndex } = action.payload;
+            const piece = state.board[rowIndex][colIndex].piece;
 
-            state.board[action.payload.rowIndex][
-                action.payload.colIndex
-            ].selected = true;
+            // Check if selected piece color and turn are the same
+            if (piece?.color === state.currentTurn) {
+                // In this case user is selecting a new piece
+
+                state.board.forEach((row) => {
+                    row.forEach((block) => {
+                        block.selected = false;
+                        block.showPath = false;
+                        block.showExplosion = false;
+                    });
+                });
+
+                state.board[rowIndex][colIndex].selected = true;
+
+                // Highlight valid moves
+
+                const moves = getValidMovesForPiece(
+                    state.board,
+                    rowIndex,
+                    colIndex,
+                    state.currentTurn,
+                    true
+                );
+
+                moves.forEach((move) => {
+                    state.board[move.row][move.col].showPath = move.danger
+                        ? false
+                        : true;
+                });
+            } else {
+                let row: number | null = null;
+                let col: number | null = null;
+
+                const selectedBlock = state.board
+                    .find((rowBlock, rowIdx) => {
+                        return (
+                            rowBlock.find((block, colIdx) => {
+                                if (block.selected) {
+                                    row = rowIdx;
+                                    col = colIdx;
+                                    return true;
+                                }
+                            }) !== undefined
+                        );
+                    })
+                    ?.find((block) => block.selected);
+
+                if (
+                    selectedBlock &&
+                    row !== null &&
+                    col !== null &&
+                    selectedBlock.piece?.color === state.currentTurn
+                ) {
+                    const move = getValidMovesForPiece(
+                        state.board,
+                        row,
+                        col,
+                        state.currentTurn,
+                        true
+                    );
+
+                    const validMove = move.find((m) => {
+                        return (
+                            m.row === rowIndex &&
+                            m.col === colIndex &&
+                            !m.danger
+                        );
+                    });
+
+                    if (validMove) {
+                        state.board.forEach((row) => {
+                            row.forEach((block) => {
+                                block.selected = false;
+                                block.showPath = false;
+                                block.showExplosion = false;
+                            });
+                        });
+                        state.board[rowIndex][colIndex].showExplosion = true;
+                        state.board[rowIndex][colIndex].piece =
+                            state.board[row][col].piece;
+
+                        // Handle pawn promotion
+                        if (
+                            state.board[rowIndex][colIndex].piece?.type ===
+                                CHESS_PIECES.PAWN &&
+                            ((state.board[rowIndex][colIndex].piece?.color ===
+                                CHESS_PIECE_COLOR.WHITE &&
+                                rowIndex === 0) ||
+                                (state.board[rowIndex][colIndex].piece
+                                    ?.color === CHESS_PIECE_COLOR.BLACK &&
+                                    rowIndex === 7))
+                        ) {
+                            state.board[rowIndex][colIndex].piece!.type =
+                                CHESS_PIECES.QUEEN;
+                        }
+
+                        state.board[row][col].piece = null;
+                    }
+                }
+            }
         },
     },
 });
