b683fc0230ae2d7412e8eb50beb5fb9f64f243de
AkashSDas <aakashdas368@gmail.com>
2024-07-27 13:05:09 +0530

    send chess start event in effect

--- frontend/src/components/match/GameSection.tsx
+++ frontend/src/components/match/GameSection.tsx
@@ -20,29 +20,86 @@ import {
     useListenMatchRoom,
     useMatchRoom,
 } from "../../hooks/match";
-import { useAppSelector } from "../../hooks/store";
-import { matchSelectors } from "../../store/match/slice";
+import { useAppDispatch, useAppSelector } from "../../hooks/store";
+import { matchActions, matchSelectors } from "../../store/match/slice";
 import { ChessBoard } from "./ChessBoard";
+import { useContext, useEffect } from "react";
+import { SocketContext } from "../../lib/websocket";
+import { MATCH_STATUS } from "../../utils/chess";
+import { useUser } from "../../hooks/auth";
+import { motion, useAnimation } from "framer-motion";
 
 export function GameSection() {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const { players } = useGetMatch();
+    const { players, matchId } = useGetMatch();
     const time = useAppSelector(matchSelectors.startTimeInMs);
+    const { socket } = useContext(SocketContext);
+    const { user } = useUser();
+    const status = useAppSelector(matchSelectors.status);
+    const controls = useAnimation();
+    const isPending =
+        status === MATCH_STATUS.IN_PROGRESS || status === MATCH_STATUS.PENDING;
+    const dispatch = useAppDispatch();
+
+    useEffect(
+        function () {
+            if (!isPending) {
+                controls.start({
+                    opacity: 0.2,
+                    filter: "grayscale(0.1)",
+                    transition: { duration: 0.5, ease: "easeInOut" },
+                });
+            } else {
+                socket?.emit("matchChessStart", { matchId });
+            }
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [isPending]
+    );
 
     useMatchRoom();
     useListenMatchRoom();
 
     return (
         <VStack w="100%" maxW="600px" gap="24px">
-            <PlayerInfo player={players!.opponent} />
+            <motion.div
+                initial={{ opacity: 1, filter: "" }}
+                animate={controls}
+                style={{ width: "100%" }}
+            >
+                <PlayerInfo player={players!.opponent} />
+            </motion.div>
 
             <ChessBoard />
 
-            <HStack w="100%" justifyContent="space-between">
+            <HStack
+                w="100%"
+                justifyContent="space-between"
+                as={motion.div}
+                initial={{ opacity: 1, filter: "" }}
+                animate={controls}
+            >
                 <HStack>
                     <Timer
                         timeInMs={time}
-                        onTimeOut={() => console.log("time out")}
+                        onTimeOut={() => {
+                            socket?.emit("matchChessEnd", {
+                                matchId,
+                                newStatus: MATCH_STATUS.TIMEOUT,
+                                metadata: { reason: "Time out" },
+                            });
+
+                            dispatch(
+                                matchActions.changeMatchStatus(
+                                    MATCH_STATUS.TIMEOUT
+                                )
+                            );
+                            dispatch(
+                                matchActions.changeMatchEndedMetadata({
+                                    reason: "Timeout",
+                                })
+                            );
+                        }}
                     />
                     <TurnText player={players!.me} />
                 </HStack>
@@ -52,7 +109,13 @@ export function GameSection() {
                 </Button>
             </HStack>
 
-            <PlayerInfo player={players!.me} />
+            <motion.div
+                initial={{ opacity: 1, filter: "" }}
+                animate={controls}
+                style={{ width: "100%" }}
+            >
+                <PlayerInfo player={players!.me} />
+            </motion.div>
 
             {/* End game modal */}
 
@@ -83,7 +146,39 @@ export function GameSection() {
                         <Button variant="contained" onClick={onClose} w="100%">
                             No
                         </Button>
-                        <Button variant="error" onClick={onClose} w="100%">
+                        <Button
+                            variant="error"
+                            onClick={() => {
+                                socket?.emit("matchChessEnd", {
+                                    matchId,
+                                    newStatus: MATCH_STATUS.CANCELLED,
+                                    metadata: {
+                                        reason: "Game ended",
+                                        byPlayer: {
+                                            username: user?.username,
+                                            id: user?.id,
+                                        },
+                                    },
+                                });
+                                onClose();
+
+                                dispatch(
+                                    matchActions.changeMatchStatus(
+                                        MATCH_STATUS.CANCELLED
+                                    )
+                                );
+                                dispatch(
+                                    matchActions.changeMatchEndedMetadata({
+                                        reason: "Game ended",
+                                        byPlayer: {
+                                            username: user!.username!,
+                                            id: user!.id!,
+                                        },
+                                    })
+                                );
+                            }}
+                            w="100%"
+                        >
                             Yes
                         </Button>
                     </ModalFooter>
