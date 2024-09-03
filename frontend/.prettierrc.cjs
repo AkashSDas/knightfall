a80c075bb4106bdb8e60e9a7d571dda418c79497
AkashSDas <aakashdas368@gmail.com>
2024-09-03 23:02:58 +0530

    add path alias and configure import sorter

--- frontend/.prettierrc.cjs
+++ frontend/.prettierrc.cjs
@@ -5,4 +5,13 @@ module.exports = {
     printWidth: 80,
     semi: true,
     singleQuote: false,
+    importOrder: [
+        "^@?\\w", // Matches third-party packages starting with @ or a-z
+        "^@/(.*)$", // Custom @/ imports
+        "^[./]", // Relative imports
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderGroupNamespaceSpecifiers: true,
+    plugins: ["@trivago/prettier-plugin-sort-imports"],
 };
