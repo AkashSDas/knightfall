c38fac7bfc5ff1651e3c5ed30b0adc9f4d5b1fc9
AkashSDas <aakashdas368@gmail.com>
2024-09-05 09:26:47 +0530

    add path aliases in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./lib/chakra";
-import { queryClient } from "./lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./lib/routes";
-import { SocketProvider } from "./providers/SocketProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { theme } from "@/lib/chakra";
+import { queryClient } from "@/lib/react-query";
+import { routes } from "@/lib/routes";
+import { SocketProvider } from "@/providers/SocketProvider";
+import { store } from "@/store";
 
 const router = createBrowserRouter(routes);
 
