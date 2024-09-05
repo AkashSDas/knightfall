939a19cf9b2b3e0068aa1e6120432f7341117d8d
AkashSDas <aakashdas368@gmail.com>
2024-09-05 23:39:55 +0530

    add image optimization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        imagetools(),
+        react(),
+        ViteImageOptimizer({
+            /* pass your config */
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
